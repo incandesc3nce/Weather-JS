9404c45cfbfd922eedc2349687b7756b605e91ea
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-02 18:42:00 +0700

    feat: add gust parameters

--- src/modules/objects/getWeatherToday.js
+++ src/modules/objects/getWeatherToday.js
@@ -7,6 +7,8 @@ export default function getWeatherToday(current) {
 
     wind_kph: current.wind_kph,
     wind_mph: current.wind_mph,
+    gust_kph: current.gust_kph,
+    gust_mph: current.gust_mph,
     wind_degree: current.wind_degree,
     wind_dir: current.wind_dir,
 

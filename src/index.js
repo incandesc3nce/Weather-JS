5f21175b7b6c438775c27e78512d7ba91720e3ee
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-04 14:19:08 +0700

    feat: hide weather when requesting for new data

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import fetchWeather from "./modules/requests/fetchWeather";
+import getWeatherToday from "./modules/objects/getWeatherToday";
+import getWeatherForecast from "./modules/objects/getWeatherForecast";
+import createWeather from "./modules/UI/createWeather";
+import fillWeatherUI from "./modules/UI/fillWeatherUI";
+import toggleWeather from "./modules/UI/toggleWeather";
+import timeoutRemoval from "./modules/weatherRemoval";
+import "./style.css";
+
+const apiKey = process.env.API_KEY;
+
+// 1 validate form
+// 2 show loading
+// 3 fetch weather
+// 4 remove loading
+// 5 fill weather
+const form = document.querySelector("form");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  if (document.querySelector(".weather")) {
+    toggleWeather(document.querySelector(".weather"));
+    await timeoutRemoval(document.querySelector(".weather"));
+  }
+
+  const city = document.querySelector("#city").value;
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&days=3&q=${city}`;
+
+  const weatherJSON = await fetchWeather(url);
+  const weatherToday = getWeatherToday(weatherJSON.current);
+  const weatherForecast = weatherJSON.forecast.forecastday.map((day) =>
+    getWeatherForecast(day),
+  );
+
+  if (!document.querySelector(".weather")) {
+    createWeather();
+    toggleWeather(document.querySelector(".weather"));
+  }
+
+  fillWeatherUI(weatherToday, weatherForecast);
+});

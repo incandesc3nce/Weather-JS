70f2d695ee73f17f676d21762dbea1449eeb1128
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-04 14:21:14 +0700

    feat: reduce timeout period for form

--- src/modules/UI/toggleWeather.js
+++ src/modules/UI/toggleWeather.js
@@ -2,7 +2,7 @@ export default function toggleWeather(weatherElement) {
   if (weatherElement.classList.contains('weather-open')) {
     weatherElement.classList.remove('weather-open');
     weatherElement.classList.add('weather-close');
-    setTimeout(() => {document.querySelector('form').classList.remove('no-bottom-round')}, 480);
+    setTimeout(() => {document.querySelector('form').classList.remove('no-bottom-round')}, 200);
   } else {
     weatherElement.classList.remove('weather-close');
     weatherElement.classList.add('weather-open');

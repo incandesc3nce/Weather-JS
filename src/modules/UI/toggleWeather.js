796a5d712f1399910eaed8b816557fa88a3a5eb2
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-04 13:50:13 +0700

    feat: show weather when data is fetched, hide when new city is entered

---
+++ src/modules/UI/toggleWeather.js
@@ -0,0 +1,11 @@
+export default function toggleWeather(weatherElement) {
+  if (weatherElement.classList.contains('weather-open')) {
+    weatherElement.classList.remove('weather-open');
+    weatherElement.classList.add('weather-close');
+    setTimeout(() => {document.querySelector('form').classList.remove('no-bottom-round')}, 480);
+  } else {
+    weatherElement.classList.remove('weather-close');
+    weatherElement.classList.add('weather-open');
+    document.querySelector('form').classList.add('no-bottom-round');
+  }
+}
